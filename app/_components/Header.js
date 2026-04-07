2b658fd7359bf3afdf4f0501531ef5f685016dff
Negar-Maleki <negarmkuk@gmail.com>
2026-04-07 11:39:54 +0100

    opt

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -3,8 +3,8 @@ import Logo from "./Logo";
 
 function Header() {
   return (
-    <header className="border-b border-primary-700 px-8 py-5 z-20 ">
-      <div className="flex justify-between items-center max-w-[90rem] mx-auto">
+    <header className="border-b border-primary-500 px-8 py-5 z-20 ">
+      <div className="flex justify-between items-center max-w-[95rem] mx-auto">
         <Logo />
         <Navigation />
       </div>
